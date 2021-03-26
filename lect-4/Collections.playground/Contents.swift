352789b0b8bba00821a1527edb5f21c3e09a7401
Aws Shkara <bass_master89@mail.ru>
2021-03-27 00:53:08 +0200

    Task completed

--- lect-4/Collections.playground/Contents.swift
+++ lect-4/Collections.playground/Contents.swift
@@ -257,12 +257,288 @@ mChSet?.remove("f")
 виведіть рядкове представлення множини mChSet.
 */
 
-var delMCHSet = mChSet?.sorted()
-delMCHSet?.remove(at: 3)
-print(delMCHSet!)
+var delMCHSet = mChSet?.firstIndex(of: "d")
+mChSet?.remove(at: delMCHSet!)
+print(mChSet!)
 
 
 /*
 8- Виведіть відстань у множині mChSet між першим елементом та
 символом а.
 */
+
+var firstElement = mChSet?.startIndex
+var lastElement = mChSet?.firstIndex(of: "a")
+mChSet?.distance(from: firstElement!, to: lastElement!)
+//-------------------------------
+
+
+/*
+9- Вставте символ а в множину mChSet.
+*/
+
+mChSet?.insert("a")
+//-------------------------------
+
+
+
+/*
+10- Опишіть множини aSet (зі значеннями One, Two, Three, 1, 2) та
+bSet (зі значеннями 1, 2, 3, One, Two).
+*/
+
+var One = 1
+var Two = 2
+var Three = 3
+
+var aSet: Set<Int> = [One, Two, Three, 1, 2]
+var bSet: Set<Int> = [1, 2, 3, One, Two]
+//-------------------------------
+
+
+/*
+11- Створіть множину, яка містить всі спільні елементи для множин
+aSet та bSet.
+*/
+
+var commonSet = aSet.intersection(bSet)
+//-------------------------------
+
+
+/*
+12- Створіть множину, яка містить унікальні елементи у множині aSet
+по відношенню до множини bSet. Створіть множину, яка містить
+унікальні елементи у множині bSet по відношенню до множини
+aSet.
+*/
+
+var uniqueAElements = aSet.subtracting(bSet)
+var uniqueBElements = bSet.subtracting(aSet)
+//-------------------------------
+
+
+/*
+13- Створіть множину, яка містить елементи, які не є спільними для
+множин aSet та bSet.
+*/
+
+var uniqueElements = bSet.symmetricDifference(aSet)
+//-------------------------------
+
+
+/*
+14- Створіть множину, яка об'єднує усі елементи множин aSet та
+bSet.
+*/
+
+var unitedElements = aSet.union(bSet)
+//-------------------------------
+
+
+/*
+15- Опишіть множини xSet (зі значеннями 2...4), ySet (зі значеннями
+1...6), zSet (зі значеннями 3, 4, 2) та x1Set (зі значеннями 5, 6, 7).
+*/
+
+var xSet: Set<Int>
+var ySet: Set<Int>
+xSet = Set(2..<4)
+ySet = Set(1..<7)
+var zSet: Set<Int> = [3, 4, 2]
+var x1Set: Set<Int> = [5, 6, 7]
+//-------------------------------
+
+
+/*
+16- Виведіть значення, які визначають чи множина xSet входить у
+множину ySet, а також чи множина ySet входить у множину xSet.
+*/
+
+
+xSet.isSubset(of: ySet)
+ySet.isSubset(of: xSet)
+//-------------------------------
+
+
+/*
+17- Виведіть значення, які визначають чи множина xSet містить
+множину ySet, а також чи множина ySet містить множину xSet.
+*/
+
+xSet.isSuperset(of: ySet)
+ySet.isSuperset(of: xSet)
+//-------------------------------
+
+
+/*
+18- Виведіть значеня, яке визначає чи множини xSet та zSet є
+рівними.
+*/
+
+xSet.elementsEqual(zSet)
+//-------------------------------
+
+
+/*
+19- Виведіть значення, яке визначає чи множина xSet входить у
+множину zSet, але не є рівною множині zSet.
+*/
+
+zSet.isStrictSuperset(of: xSet)
+//-------------------------------
+
+
+/*
+20- Виведіть значення, яке визначає чи множина xSet містить
+множину zSet, але не є рівною множині zSet.
+*/
+
+xSet.isStrictSuperset(of: zSet)
+//-------------------------------
+
+
+
+
+
+
+// Third task - Dictionaries
+
+
+/*
+1- Опишіть словник nDict, ключами якого є числові рядкові
+предсталення чисел від 1 до 5, а відповідними значеннями є
+рядкові представлення чисел від 1 до 5 на англійській мові
+(наприклад, 1:”One”).
+*/
+
+var nDict = [1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "five"]
+//-------------------------------
+
+
+/*
+2- Виведіть на екран значення масиву nDict за ключем 3.
+*/
+
+nDict[3]
+//-------------------------------
+
+
+/*
+3- Виведіть на екран значення масиву nDict за індексом ключа 4.
+*/
+
+nDict.index(forKey: 4)
+//-------------------------------
+
+
+/*
+4- Створіть mutable словник mNDict на основі словника nDict.
+*/
+
+
+var mNDict: [Int:String]
+
+mNDict = nDict
+//-------------------------------
+
+
+/*
+5- Додайте елементи 6:”Seven” та 7:”Six” до словника mNDict.
+*/
+
+mNDict[6] = "Seven"
+mNDict[7] = "Six"
+//-------------------------------
+
+
+/*
+6- Оновіть значення елементів словника mNDict, не використовуючи
+subscript [], до наступних: 6:”Six”, 7:”Seven”, 8:”Eight”.
+*/
+
+mNDict.updateValue("Six", forKey: 6)
+mNDict.updateValue("Seven", forKey: 7)
+mNDict.updateValue("Eight", forKey: 8)
+//-------------------------------
+
+
+
+/*
+7- Видаліть елемент за ключем 5 зі словника mNDict.
+*/
+
+mNDict.removeValue(forKey: 5)
+//-------------------------------
+
+
+/*
+8- Видаліть елемент за індексом ключа 4 зі словника mNDict.
+*/
+
+mNDict.remove(at: mNDict.index(forKey: 4)!)
+//-------------------------------
+
+
+/*
+9- Визначіть та виведіть на екран відстань у словнику mNDict між
+парами значень 1:”One” та 7:”Seven”.
+*/
+
+var elementOne = mNDict.index(forKey: 1)
+var elementSeven = mNDict.index(forKey: 7)
+
+if elementOne! < elementSeven! {
+	
+	mNDict.distance(from: elementOne!, to: elementSeven!)
+}
+
+if elementSeven! < elementOne! {
+	
+	mNDict.distance(from: elementSeven!, to: elementOne!)
+}
+//-------------------------------
+
+
+/*
+10- Створіть масив mNDictKeys, елементами якого є усі ключі
+словника mNDict.
+*/
+
+var mNDictKeys: Array<Int>
+mNDictKeys = Array(mNDict.keys)
+//-------------------------------
+
+
+/*
+11- Створіть масив mNDictKeys, елементами якого є усі значення
+словника mNDict.
+*/
+
+var mNDictValues: Array<String>
+mNDictValues = Array(mNDict.values)
+//-------------------------------
+
+
+/*
+12- Виведіть на екран кількість елементів словника mNDict, а також
+кількість його всіх ключів та його всіх значень.
+*/
+
+mNDict.count
+
+var mnDictKeys = mNDict.keys
+mNDictKeys.count
+
+var mNDictKeyValue = mNDict.values
+mNDictKeyValue.count
+//-------------------------------
+
+
+/*
+13- Виведіть на екран рядкове представлення словника mNDict.
+*/
+
+// Не до конца понял что тут конкретно требуется
+
+print("\(mNDict)")
+//-------------------------------
